7560270122894a2329ee6684d4f90dc3e3965e31
Mikail Fener <mika1292@edu.easj.dk>
2019-06-12 02:21:47 +0200

    Added Stuff

---
+++ TCPServer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TcpListener serverSocket = new TcpListener(6789);
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TcpListener serverSocket = new TcpListener(ip, 6789);
+
+            Console.WriteLine("Server started and then the main thread is blocked");
+            serverSocket.Start();
+
+
+            TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+            Console.WriteLine("Server activated");
+            //NetworkStream ns = new NetworkStream(connectionSocket);
+            //only on real sockets not TcpClient
+            Stream ns = connectionSocket.GetStream();
+
+            //StreamReader læser karather fra en byte stream
+            StreamReader sr = new StreamReader(ns);
+            //StreamWriter skriv karather til en stream
+            StreamWriter sw = new StreamWriter(ns);
+            //Gets or sets a value indicating whether the StreamWriter will flush its buffer to the underlying stream after every call to StreamWriter.Write
+            sw.AutoFlush = true;
+
+            while (true)
+            {
+                string message = sr.ReadLine();
+                Console.WriteLine("Client: " + message);
+                if (message == "STOP") break;
+                string answer = Console.ReadLine();
+                sw.WriteLine(answer);
+            }
+
+            Console.WriteLine("Server stopped, press ENTER");
+            string s = Console.ReadLine();
+
+            ns.Close();
+            connectionSocket.Close();
+            serverSocket.Stop();
+        }
+    }
+}
--- TCPServerAndClient/Program.cs
+++ TCPServerAndClient/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net.Sockets;
 
 namespace TCPServerAndClient
 {
@@ -6,7 +8,32 @@ namespace TCPServerAndClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Provides client connections for TCP network services
+            TcpClient clientSocket = new TcpClient("127.0.0.1", 6789);
+            Console.WriteLine("Client ready");
+
+            Stream ns = clientSocket.GetStream();  //provides a Stream
+
+            //StreamReader læser karather fra en byte stream
+            StreamReader sr = new StreamReader(ns);
+            //StreamWriter skriv karather til en stream
+            StreamWriter sw = new StreamWriter(ns);
+            //Gets or sets a value indicating whether the StreamWriter will flush its buffer to the underlying stream after every call to StreamWriter.Write
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = Console.ReadLine();
+            sw.WriteLine(message);
+            string serverAnswer = sr.ReadLine();
+
+            Console.WriteLine("Server: " + serverAnswer);
+
+            Console.WriteLine("No more from server. Press Enter");
+            Console.ReadLine();
+
+            ns.Close();
+
+            clientSocket.Close();
+
         }
     }
 }
